b6af11da6babb253ee7f0fed5a017cd4d6aa01e3
Hafsa Hameed <hafsa.hameed457@gmail.com>
2023-09-07 16:56:23 +0500

    settingup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-
-import "./App.css";
+import Landing from "./pages/Landing";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div>
+      <Landing></Landing>
+      hehe
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactComponent as Logo } from "../assets/images/logo.svg";
+// import { ReactComponent as Main } from "../assets/images/main.svg";
+const Landing = () => {
+  return (
+    <div>
+      <Logo />
+      heheh
+    </div>
+  );
+};
+
+export default Landing;
